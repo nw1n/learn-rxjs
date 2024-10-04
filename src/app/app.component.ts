afe413cf02d487dd18ebcf0ae00b101f37e5adfb
Nicolas Wanner <n.wanner@elderbyte.com>
2024-10-04 21:47:56 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,128 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { TryOneComponent } from './try-one/try-one.component';
-import { TryTwoComponent } from './try-one/try-two.component';
+import { Component, Signal, ViewChild } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import {
+  BehaviorSubject,
+  concat,
+  fromEvent,
+  interval,
+  map,
+  mapTo,
+  Observable,
+  of,
+  take,
+  tap,
+  timer,
+} from 'rxjs';
+
+@Component({
+  selector: 'app-try-two',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Try Two Component</h1>
+    <div>valueA: {{ valueA }}</div>
+  `,
+})
+export class TryTwoComponent {
+  valueA = 0;
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log('TryTwoComponent');
+    this.regularWay();
+    this.rxjsWay();
+  }
+
+  private regularWay() {
+    // define a function
+    const myFunction = () => {
+      console.log('reg1');
+      console.log('reg2');
+      console.log('reg3');
+    };
+
+    // call the function
+    myFunction();
+  }
+
+  private rxjsWay() {
+    // define an observable
+    const myObservable$ = new Observable((subscriber) => {
+      subscriber.next('rx1');
+      subscriber.next('rx2');
+      subscriber.next('rx3');
+    });
+
+    // "call" the observable
+    const mySubcription = myObservable$.subscribe((value) => {
+      console.log(value);
+    });
+
+    // unsubscribe
+    mySubcription.unsubscribe();
+  }
+}
+
+@Component({
+  selector: 'app-try-three',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Try Three Component</h1>
+    <div>valueA: {{ valueA }}</div>
+  `,
+})
+export class TryThreeComponent {
+  valueA = 0;
+
+  constructor() {}
+
+  ngOnInit() {
+    window.setTimeout(() => {
+      console.log('TryThreeComponent');
+      //this.regularWay();
+      this.rxjsWay();
+    }, 100);
+  }
+
+  private regularWay() {
+    // define a function
+    const myFunction = () => {
+      window.setTimeout(() => {
+        console.log('reg1');
+        window.setTimeout(() => {
+          console.log('reg2');
+          window.setTimeout(() => {
+            console.log('reg3');
+          }, 100);
+        }, 100);
+      }, 100);
+    };
+
+    // call the function
+    myFunction();
+  }
+
+  private rxjsWay() {
+    const interval100ms$ = interval(100);
+
+    //const takeFourNumbers = numbers.pipe(take(3));
+    const interval100msTakeThree$ = interval100ms$.pipe(take(3));
+
+    const sub = interval100msTakeThree$.subscribe((x) =>
+      console.log('rx' + (x + 1))
+    );
+
+    // we don't need to unsubscribe because take(3) will complete the observable
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TryTwoComponent],
-  template: `
-    <h1>{{ title }}</h1>
-    <app-try-two></app-try-two>
-  `,
+  imports: [CommonModule, TryTwoComponent, TryThreeComponent],
+  template: `<app-try-three></app-try-three> `,
 })
-export class AppComponent {
-  title = 'learn-rxjs';
-
-  constructor() {
-    console.log('AppComponent constructor');
-  }
-}
+export class AppComponent {}
