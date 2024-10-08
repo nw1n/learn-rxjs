342767dfad7b6c421aee46c6ee73ff05eb6d6cb9
Nicolas Wanner <n.wanner@elderbyte.com>
2024-10-08 23:59:09 +0200

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,78 +24,12 @@ import { SafeSubscriber, Subscriber } from 'rxjs/internal/Subscriber';
   template: `
     <h1>Example</h1>
     <p>myTemplateVal {{ myTemplateVal }}</p>
-    <p>myTemplateValAsync {{ $executeThrice | async }}</p>
-    <p>myTemplateValSignal {{ executeThriceSignal() }}</p>
-    <p>myTemplateValSignalTwice {{ executeTwiceSignal() }}</p>
-    <p>myTemplateValSignalTwiceDouble {{ executeTwiceDoubleSignal() }}</p>
-    <p>myTemplateValAnotherObservable {{ $anotherObservable | async }}</p>
   `,
 })
 export class ExampleComponent {
   myTemplateVal = 0;
-  $executeThrice!: Observable<any>;
-  executeThriceSignal!: Signal<any>;
-  executeTwiceSignal!: Signal<any>;
-  executeTwiceDoubleSignal!: Signal<any>;
-  $anotherObservable!: Observable<any>;
 
-  constructor() {
-    console.log('this start of constructor');
-    const $executeThrice = new Observable((observer: Observer<number>) => {
-      observer.next(1);
-      observer.next(2);
-      observer.next(3);
-      observer.complete();
-    }).pipe(tap((value) => console.log(value)));
-    // pipe tap console.log will be called before 'this end of constructor'
-    // because of toSignal
-    this.executeThriceSignal = toSignal($executeThrice);
-
-    const $executeTwice = $executeThrice.pipe(take(2));
-    this.executeTwiceSignal = toSignal($executeTwice);
-
-    const $executeTwiceDouble = $executeTwice.pipe(map((value) => value * 2));
-    this.executeTwiceDoubleSignal = toSignal($executeTwiceDouble);
-
-    this.$anotherObservable = new Observable((observer: Observer<any>) => {
-      observer.next('A');
-      observer.next('B');
-      observer.complete();
-    }).pipe(tap((value) => console.log(value)));
-    // this will be called after 'this end of constructor', when the template is rendered
-
-    console.log('this end of constructor');
-  }
-
-  ngOnInit() {
-    //this.baseWay();
-    this.otherWay();
-  }
-
-  otherWay() {}
-
-  baseWay() {
-    const $executeThrice = new Observable((observer: Observer<number>) => {
-      observer.next(1);
-      observer.next(2);
-      observer.next(3);
-      observer.complete();
-    });
-    this.$executeThrice = $executeThrice;
-
-    $executeThrice.subscribe({
-      next: (value) => console.log(value),
-      complete: () => console.log('Complete 1'),
-    });
-
-    $executeThrice.subscribe({
-      next: (value) => {
-        this.myTemplateVal = value;
-        console.log(value);
-      },
-      complete: () => console.log('Complete 2'),
-    });
-  }
+  constructor() {}
 }
 
 @Component({
