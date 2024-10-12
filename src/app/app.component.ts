a46ff49605f50746b8caa300f35a574055b26f49
Nicolas Wanner <n.wanner@elderbyte.com>
2024-10-12 17:48:27 +0200

    cl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  computed,
   EventEmitter,
   input,
+  model,
   Signal,
   ViewChild,
 } from '@angular/core';
@@ -28,14 +30,100 @@ import {
   timer,
 } from 'rxjs';
 import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'child-component',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <h1>Child</h1> `,
+  imports: [CommonModule, FormsModule],
+  template: `
+    <h1>Child</h1>
+    <input type="text" [(ngModel)]="title" />
+    <p>{{ titleUpperCase() }}</p>
+    <input
+      type="text"
+      [ngModel]="countAsText()"
+      (ngModelChange)="handleTextChange($event)"
+    />
+    <p>countAsText: {{ countAsText() }}</p>
+    <p>countAsNumber: {{ countAsNumber() }}</p>
+    <button (click)="incrementNumber()">increment</button>
+  `,
 })
-export class TheChildComponent {}
+export class TheChildComponent {
+  title = model('default-title');
+  titleUpperCase = computed(() => this.title().toUpperCase());
+  countAsText = model('zero');
+  countAsNumber = computed<number>(() => {
+    return nrStrToNr(this.countAsText());
+  });
+
+  incrementNumber() {
+    let newValue = this.countAsNumber() + 1;
+    if (newValue > 9) {
+      newValue = 0;
+    }
+    this.countAsText.set(nrToStr(newValue));
+  }
+
+  handleTextChange(value: string) {
+    this.countAsText.set(value);
+  }
+}
+
+function nrToStr(nr: number): string {
+  switch (nr) {
+    case 0:
+      return 'zero';
+    case 1:
+      return 'one';
+    case 2:
+      return 'two';
+    case 3:
+      return 'three';
+    case 4:
+      return 'four';
+    case 5:
+      return 'five';
+    case 6:
+      return 'six';
+    case 7:
+      return 'seven';
+    case 8:
+      return 'eight';
+    case 9:
+      return 'nine';
+    default:
+      return 'zero';
+  }
+}
+
+function nrStrToNr(str: string): number {
+  switch (str) {
+    case 'zero':
+      return 0;
+    case 'one':
+      return 1;
+    case 'two':
+      return 2;
+    case 'three':
+      return 3;
+    case 'four':
+      return 4;
+    case 'five':
+      return 5;
+    case 'six':
+      return 6;
+    case 'seven':
+      return 7;
+    case 'eight':
+      return 8;
+    case 'nine':
+      return 9;
+    default:
+      return NaN;
+  }
+}
 
 @Component({
   selector: 'example-component',
