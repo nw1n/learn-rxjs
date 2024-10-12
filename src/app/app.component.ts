0992cf42c21671951ccdfbed27e6cb1bb00a2bd3
Nicolas Wanner <n.wanner@elderbyte.com>
2024-10-12 22:33:09 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,6 +85,25 @@ export class TheChildComponent {
   }
 }
 
+@Component({
+  selector: 'example-component',
+  standalone: true,
+  imports: [CommonModule, TheChildComponent],
+  template: `
+    <h1>Example</h1>
+    <child-component></child-component>
+  `,
+})
+export class ExampleComponent {}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, ExampleComponent],
+  template: `<example-component></example-component>`,
+})
+export class AppComponent {}
+
 function nrToStr(nr: number): string {
   switch (nr) {
     case 0:
@@ -108,7 +127,7 @@ function nrToStr(nr: number): string {
     case 9:
       return 'nine';
     default:
-      return 'zero';
+      return 'Nan';
   }
 }
 
@@ -138,22 +157,3 @@ function nrStrToNr(str: string): number {
       return NaN;
   }
 }
-
-@Component({
-  selector: 'example-component',
-  standalone: true,
-  imports: [CommonModule, TheChildComponent],
-  template: `
-    <h1>Example</h1>
-    <child-component></child-component>
-  `,
-})
-export class ExampleComponent {}
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, ExampleComponent],
-  template: `<example-component></example-component>`,
-})
-export class AppComponent {}
